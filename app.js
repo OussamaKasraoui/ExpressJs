4b38377af803d86de8fae5018bac62db3b9c21ee
OussamaKasraoui <kasraouioussama@gmail.com>
2019-11-09 01:27:54 +0100

    Update app.js
    related to heroku Error R10 (Boot timeout)

--- app.js
+++ app.js
@@ -13,8 +13,13 @@ const app=express();
       app.use(routes);
       require('dotenv').config({path: __dirname + '/.env'});
       require('./core/passport')(passport);
+
+// related to heroku Error R10 (Boot timeout) -> Web process failed to bind to $PORT within 60 seconds of launch
+// Solved @ https://stackoverflow.com/questions/31092538/heroku-node-js-error-r10-boot-timeout-web-process-failed-to-bind-to-port-w
+app.set('port', (process.env.APP_PORT || 5000));
+
 //init
-app.listen(process.env.APP_PORT || 5000, () => console.info("Back-End server running on port : "+ process.env.APP_PORT));
+app.listen(app.get('port'), () => console.info("Back-End server running on port : "+ process.env.APP_PORT));
 
 //connect To Database
  mongoose.connect(process.env.DB_URI, {useNewUrlParser: true});
